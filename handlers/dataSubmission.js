89326ceba8b4ec99928439ca60391b27b8c4859f
Jessica Deacon <j.r.deacon@hotmail.co.uk>
2020-08-19 14:47:37 +0100

    created login and signup handlers
    relates to #16
    
    Co-authored-by: My Hoa Tien <myhoatien@gmail.com>

---
+++ handlers/dataSubmission.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const path = require("path");
+
+
+function getLoginHandler() {
+
+}
+
+function postLoginHandler() {
+
+}
+
+function getSignUpHandler() {
+
+}
+
+function postSignUpHandler() {
+    
+}
+
+module.exports = {getLoginHandler,postLoginHandler, getSignUpHandler, postSignUpHandler};
--- router.js
+++ router.js
@@ -6,6 +6,7 @@ const publicHandler = require("./handlers/public");
 const missingHandler = require("./handlers/missing");
 const getPostsHandler = require("./handlers/getPosts");
 const submitHandler = require("./handlers/submit");
+const dataSubmission = require("./handlers/dataSubmission");
 
 function router(request, response) {
     const url = request.url;
@@ -17,8 +18,16 @@ function router(request, response) {
         publicHandler(request, response);
     } else if (url.includes("submit") && method === "POST") {
         submitHandler(request, response);
-    } else if (url.includes("get-posts")){
-        getPostsHandler(request, response);    
+    } else if (url.includes("get-posts")) {
+        getPostsHandler(request, response);
+    } else if (url.includes("login") && method === "POST") {
+        dataSubmission.postLoginHandler(request, response);
+    } else if (url.includes("login") && method === "GET") {
+        dataSubmission.getLoginHandler(request, response);
+    } else if (url.includes("sign-up") && method === "POST") {
+        dataSubmission.postSignUpHandler(request, response);
+    } else if (url.includes("sign-up") && method === "GET") {
+        dataSubmission.getSignUpHandler(request, response);
     } else {
         missingHandler(request, response);
     }
