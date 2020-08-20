5da4de34f0247003ebba88cf3a17e3b570f7e39e
jenndroid <jennifer.sesaybarnes@gmail.com>
2020-08-20 21:05:20 +0100

    Logout complete

--- handlers/logout.js
+++ handlers/logout.js
@@ -3,16 +3,14 @@ const path = require("path");
 
 
 function logoutHandler(request, response){
-    res.writeHead(
-        302,
+    response.writeHead(
+        200,
         {
-          'Location': '/',
           'Set-Cookie': 'jwt=0; Max-Age=0',
           'Content-Type': 'text/html',
         }
       );
-      return res.end("<h1>You have logged out</h1>");
+    return response.end("<h1>You have logged out</h1>");
 }
-
 
 module.exports = logoutHandler;
