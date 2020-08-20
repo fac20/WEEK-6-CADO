6c225ffb8080ce0c33250b7586aa492d83a25b82
jenndroid <jennifer.sesaybarnes@gmail.com>
2020-08-20 21:00:21 +0100

    Overwrites cookie on successful logout

--- handlers/logout.js
+++ handlers/logout.js
@@ -3,16 +3,15 @@ const path = require("path");
 
 
 function logoutHandler(request, response){
-    // const filePath = path.join(__dirname, );
-    // fs.readFile(filePath, (error, file) => {
-        if (error){
-            console.error(error);
-            response.writeHead(404, {"content-type" : "text/html"});
-            response.end("<h1>Not Found</h1>");
-        } else {
-            response.writeHead(302, { 'location': '/', 'Set-Cookie': 'logged_in=0 ; Max-Age=0' });    // colon or semi colon??
-          response.end();
+    res.writeHead(
+        302,
+        {
+          'Location': '/',
+          'Set-Cookie': 'jwt=0; Max-Age=0',
+          'Content-Type': 'text/html',
         }
+      );
+      return res.end("<h1>You have logged out</h1>");
 }
 
 
