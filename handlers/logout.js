8aa486ef881850850bbba4044eb0453e3ed33933
Aishah <aissshah@outlook.com>
2020-08-20 21:22:34 +0100

    Nav bar
    Co-authored-by: My Hoa Tien <myhoatien@gmail.com>

--- handlers/logout.js
+++ handlers/logout.js
@@ -5,16 +5,14 @@ const path = require("path");
 function logoutHandler(request, response){
     // const filePath = path.join(__dirname, );
     // fs.readFile(filePath, (error, file) => {
-        if (error){
-            console.error(error);
-            response.writeHead(404, {"content-type" : "text/html"});
-            response.end("<h1>Not Found</h1>");
-        } else {
-            response.writeHead(302, { 'location': '/', 'Set-Cookie': 'logged_in=0 ; Max-Age=0' });    // colon or semi colon??
+        // if (error){
+        //     console.error(error);
+        //     response.writeHead(404, {"content-type" : "text/html"});
+        //     response.end("<h1>Not Found</h1>");
+        // } else {
+        response.writeHead(302, { 'location': '/', 'Set-Cookie': 'logged_in=0 ; Max-Age=0' });   
           response.end();
-        }
-});
-
+        // }
 }
 
 module.exports = logoutHandler;
