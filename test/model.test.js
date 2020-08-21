b46a23c52c35abb37ca0f5b37f5028e6212c2ef3
jenndroid <jennifer.sesaybarnes@gmail.com>
2020-08-21 08:24:41 +0100

    First test to get all usernames

--- test/model.test.js
+++ test/model.test.js
@@ -2,3 +2,15 @@ const path = require("path");
 const build = require("..", "database", "build");
 const test = require("tape");
 const { getPostsData, add, createUser, getUser, getAllUsernames } = require("..", "handlers", "model");
+
+//test for getting all usernames
+test("Can get all usernames", (t) => {
+    build().then(() => {
+      getAllUsernames().then((users) => {
+        const firstUser = users[0];
+        t.equal(firstUser.username, "Kitty");
+        t.equal(firstUser.location, "Your Heart");
+        t.end();
+      });
+    });
+  });
