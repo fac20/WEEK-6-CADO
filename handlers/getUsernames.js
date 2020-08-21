6394e93a3fdac17e908b0d099f52d84e6fc54f3a
mhtien <59407030+mhtien@users.noreply.github.com>
2020-08-21 12:46:55 +0100

    fix merge issue

--- handlers/getUsernames.js
+++ handlers/getUsernames.js
@@ -3,20 +3,6 @@ const path = require('path');
 const model = require('./model');
 
 function getUsernamesHandler(request, response) {
-<<<<<<< HEAD
-  model.getAllUsernames()
-  .then((result) => {
-    const users = result.rows;
-    response.writeHead(200, { "content-type": "text/html" });
-    response.end(`${JSON.stringify(users)}`);
-  })
-  .catch((error) => {
-    console.log(error);
-    response.writeHead(500, {"content-type":"text/html"});
-    response.end("<h1>Something went wrong</h1>")
-  })
-};
-=======
 	model
 		.getAllUsernames()
 		.then(result => {
@@ -30,6 +16,5 @@ function getUsernamesHandler(request, response) {
 			response.end('<h1>Something went wrong</h1>');
 		});
 }
->>>>>>> master
 
 module.exports = getUsernamesHandler;
