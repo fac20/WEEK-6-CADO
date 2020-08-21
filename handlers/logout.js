a0deb610b09020d2d6502735347ac0ab03a3b2a7
jenndroid <jennifer.sesaybarnes@gmail.com>
2020-08-21 12:21:53 +0100

    Home button redirects after logout

--- handlers/logout.js
+++ handlers/logout.js
@@ -1,14 +1,29 @@
 const fs = require('fs');
 const path = require('path');
-
 const { parse } = require('cookie');
 const { sign, verify } = require('jsonwebtoken');
+
+function logoutTemplate() {
+	return `
+	<h1>You have logged out</h1>
+	<button onclick="window.location.href='/'">Home</button>
+	`;
+}
+
 function logoutHandler(request, response) {
+	//check user is logged in first
+	if (!request.headers.cookie) {
+		response.writeHead(302, {
+			Location: '/',
+		});
+		return response.end();
+	}
+
 	response.writeHead(200, {
 		'Set-Cookie': 'jwt=0; Max-Age=0',
 		'Content-Type': 'text/html',
 	});
-	return response.end('<h1>You have logged out</h1>');
+	return response.end(logoutTemplate());
 }
 
 module.exports = logoutHandler;
